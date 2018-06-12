535e23ca9177f89d4fcff924ff94efa54d507356
Louis Litt <louislittosbot@gmail.com>
2018-06-12 16:16:46 +0100

    create attacklist class

---
+++ src/settings/AttackList.java
@@ -0,0 +1,14 @@
+package settings;
+
+public class AttackList {
+
+    private String[] npcs;
+
+    public String[] getNpcs() {
+        return npcs;
+    }
+
+    public void setNpcs(String[] npcs) {
+        this.npcs = npcs;
+    }
+}
