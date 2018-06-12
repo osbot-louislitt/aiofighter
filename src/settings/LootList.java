bc30245a625d1c9d93c0f4fccfa9f9f792049715
Louis Litt <louislittosbot@gmail.com>
2018-06-12 16:16:58 +0100

    Add lootlist class

---
+++ src/settings/LootList.java
@@ -0,0 +1,14 @@
+package settings;
+
+public class LootList {
+
+    private String[] items;
+
+    public String[] getItems() {
+        return items;
+    }
+
+    public void setItems(String[] items) {
+        this.items = items;
+    }
+}
